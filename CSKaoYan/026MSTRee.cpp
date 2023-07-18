1feb143a53586451df1510c98b54720cd2e617b8
chriswoodcn <chriswoodcn@aliyun.com>
2023-07-18 17:33:06 +0800

    [update] MST

---
+++ CSKaoYan/026MSTRee.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: chriswoodcn
+ * @Email: chriswoodcn@aliyun.com
+ * @Date: 2023-07-18 17:24:32
+ * @LastEditors: chriswoodcn
+ * @LastEditTime: 2023-07-18 17:31:46
+ * @Description: 最小生成树
+ * 
+ * Copyright (c) 2023 by chriswoodcn, All Rights Reserved. 
+ */
+// -----------------------------------------最小生成树-----------------------------------------------
+// 生成树
+// 连通图的生成树是包含图中全部顶点的一个极小连通子图(n个顶点的图G,n-1条边,连通)
+
+// 最小生成树(最小代价树)Minimum Spanning Tree
+// 最小生成树可能有多个,但边的权值之和是唯一且最小的
+// 最小生成树的边数: 顶点数 - 1
