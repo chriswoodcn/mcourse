07d9b59873e5c09125effe8f7defe8f6957e329d
chriswoodcn <chriswoodcn@aliyun.com>
2024-03-13 12:48:14 +0800

    [update] format style change

--- CCourse/io/standard_io.c
+++ CCourse/io/standard_io.c
@@ -1,43 +1,30 @@
 #include <stdio.h>
-void compute_buff_size_1()
-{
-  for (int i = 0; i < 1025; i++)
-  {
-    if (i < 10)
-    {
+void compute_buff_size_1() {
+  for (int i = 0; i < 1025; i++) {
+    if (i < 10) {
       printf("000%d", i);
-    }
-    else if (i < 100)
-    {
+    } else if (i < 100) {
       printf("00%d", i);
-    }
-    else if (i < 1000)
-    {
+    } else if (i < 1000) {
       printf("0%d", i);
-    }
-    else
-    {
+    } else {
       printf("%d", i);
     }
   }
   // 通过printf结合打印出来的字符计算出缓存区大小为
   // 1024 * 4 = 4096字节
-  while (1)
-  {
+  while (1) {
   }
 }
-void compute_buff_size_2()
-{
+void compute_buff_size_2() {
   printf("");
   // ubuntu18.04
   // int stdout_size = stdout->_IO_buf_end - stdout->_IO_buf_base;
 }
-int test_fopen_1()
-{
+int test_fopen_1() {
   FILE *fp = NULL;
   fp = fopen("./test.txt", "r");
-  if (fp == NULL)
-  {
+  if (fp == NULL) {
     printf("fopen fail\n");
     return -1;
   }
@@ -45,12 +32,10 @@ int test_fopen_1()
   return 0;
 }
 
-int test_fopen_2()
-{
+int test_fopen_2() {
   FILE *fp = NULL;
   fp = fopen("./test.txt", "w+");
-  if (fp == NULL)
-  {
+  if (fp == NULL) {
     printf("fopen fail\n");
     return -1;
   }
@@ -58,12 +43,10 @@ int test_fopen_2()
   int res = fclose(fp);
   printf("fclose res = %d\n", res);
 }
-int test_fopen_3()
-{
+int test_fopen_3() {
   FILE *fp = NULL;
   fp = freopen("./test.txt", "a+", stdout);
-  if (fp == NULL)
-  {
+  if (fp == NULL) {
     printf("freopen fail\n");
     return -1;
   }
@@ -72,8 +55,7 @@ int test_fopen_3()
   fclose(fp);
   return 0;
 }
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   // printf("hello standard io\n");
   // compute_buff_size_1();
   // test_fopen_1();
