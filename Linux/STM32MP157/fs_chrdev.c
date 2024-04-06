e0234fb6598840de6ec1d6fefeddf845f366de37
chriswoodcn <chriswoodcn@aliyun.com>
2024-04-06 17:50:41 +0800

    [update] 字符设备补充

--- Linux/STM32MP157/fs_chrdev.c
+++ Linux/STM32MP157/fs_chrdev.c
@@ -31,11 +31,86 @@ static int fs_chrdev_release(struct inode *inode, struct file *file) {
   /*具体功能实现*/
   return 0;
 }
+// 使用 copy_to_user、copy_from_user实现用户数据和内核数据的交互而不是简单的通过
+// mencpy 拷贝
+/*定义数据缓存区*/
+#define BUFF_SIZE 128
+static char dev_data[BUFF_SIZE] = {'\0'};
+static int current_size = 0;
+/*write接口*/
+static ssize_t fs_chrdev_write(struct file *file, const char __user *buff,
+                               size_t size, loff_t *loff) {
+  int count = 0;
+
+  /*检查size的正确性*/
+  if (size < 0) {
+    return -EINVAL;
+  } else if (size > BUFF_SIZE - 1) {
+    count = BUFF_SIZE;
+  } else {
+    count = size;
+  }
+
+  /*将用户空间数据拷贝到内核空间*/
+  if (copy_from_user(dev_data, buff, count)) {
+    return -EFAULT;
+  }
+
+  dev_data[count] = '\0';
+  current_size = count;
+
+  return count;
+}
+/*read接口*/
+static ssize_t fs_chrdev_read(struct file *file, char __user *buff, size_t size,
+                              loff_t *loff) {
+  int count = 0;
+
+  /*检查size的正确性*/
+  if (size < 0) {
+    return -EINVAL;
+  } else if (size > current_size) {
+    count = current_size;
+  } else {
+    count = size;
+  }
+
+  /*将内核数据拷贝到用户空间*/
+  if (copy_to_user(buff, dev_data, count)) {
+    return -EFAULT;
+  }
+
+  current_size = 0;
+
+  return count;
+}
+/*ioctl接口*/
+static long fs_chrdev_unlocked_ioctl(struct file *inode, unsigned int cmd,
+                                     unsigned long arg) {
+  printk(KERN_INFO "fs_chrdev: fs_chrdev_unlocked_ioctl\n");
+
+  switch (cmd) {
+  case FS_CMD1:
+    printk(KERN_INFO "fs_chrdev: FS_CMD1\n");
+    break;
+  case FS_CMD2:
+    printk(KERN_INFO "fs_chrdev: FS_CMD2\n");
+    break;
+  default:
+    printk(KERN_INFO "fs_chrdev: invalid argment\n");
+  }
+
+  return 0;
+}
+
 /*用户操作接口集合*/
 static struct file_operations fs_chrdev_fops = {
     .owner = THIS_MODULE,
     .open = fs_chrdev_open,
     .release = fs_chrdev_release,
+    .read = fs_chrdev_read,
+    .write = fs_chrdev_write,
+    .unlocked_ioctl = fs_chrdev_unlocked_ioctl,
 };
 
 /*加载函数*/
--- Linux/STM32MP157/test_fs_chrdev.c
+++ Linux/STM32MP157/test_fs_chrdev.c
@@ -1,7 +1,17 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-
+/**
+ * open 设备文件的时候可能出现的错误
+ * 1）No such file or directory：没有文件或目录
+ * 错误原因：打开的设备文件不存在，检查设备文件是否存在，文件名是否正确
+ * 2）Permission denied：权限问题
+ * 错误原因：设备打开需要超级用户权限，修改设备文件权限或使用 sudo 执行应用程序
+ * 3）No such device or address：没有设备或地址
+ * 错误原因：驱动未加载或创建设备文件的时候设备号与驱动中设备号不一致
+ */
 int main(int argc, const char *argv[]) {
   int fd;
 
@@ -10,7 +20,26 @@ int main(int argc, const char *argv[]) {
     perror("open");
     return -1;
   }
-
+  char buf[128] = "Hello World";
+  while (1) {
+    memset(buf, 0, sizeof(buf));
+    scanf("%s", buf);
+    /*写入键盘数据的数据*/
+    nbyte = write(fd, buf, strlen(buf));
+    if (nbyte < 0) {
+      perror("write");
+      return -1;
+    }
+    printf("write: nbyte = %d\n", nbyte);
+    /*清空buf，读取数据存放在buf中*/
+    memset(buf, 0, sizeof(buf));
+    nbyte = read(fd, buf, sizeof(buf));
+    if (nbyte < 0) {
+      perror("read");
+      return -1;
+    }
+    printf("read: nbyte = %d, buf = %s\n", nbyte, buf);
+  }
   close(fd);
 
   return 0;
