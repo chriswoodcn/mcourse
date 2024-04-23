f9a76f0b7068168fb41b22d100a0432bc8a876ec
chriswoodcn <chriswoodcn@aliyun.com>
2024-04-23 23:13:57 +0800

    [update] i2c理论

--- STM32/89C52RC/src/013_oled/main.c
+++ STM32/89C52RC/src/013_oled/main.c
@@ -5,11 +5,13 @@ void main()
 {
 	OLED_Init();
 	OLED_ShowChar(1,1,'A');
+	OLED_ShowChar(1,3,'a');
 	OLED_ShowString(1,4,"YourFun!");
 	OLED_ShowNum(2,1,2397,4);
 	OLED_ShowSignedNum(2,7,-2397,4);
 	OLED_ShowHexNum(3,1,0xff01,4);
-	OLED_ShowBinNum(4,1,1111111100000001,16);
+	// OLED_ShowBinNum(4,1,1111111100000001,16);
+	OLED_ShowBinNum(4,1,0xff01,16);
 	while(1)
 	{
 
