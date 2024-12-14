cdd1f716320baf3a49e48d4a7ba429a960e8d6f8
chriswoodcn <chriswoodcn@aliyun.com>
2024-12-14 23:28:52 +0800

    [update] CmakePro init

---
+++ CmakePro/101/xlog/xlog.h
@@ -0,0 +1,18 @@
+// xlog.h
+#ifndef XLOG_H
+#define XLOG_H
+
+#ifndef _WIN32
+#define XCPP_API
+#else
+#ifdef xlog_EXPORTS
+#define XCPP_API __declspec(dllexport)
+#else
+#define XCPP_API __declspec(dllimport)
+#endif
+#endif
+class XCPP_API XLog {
+public:
+  XLog();
+};
+#endif
