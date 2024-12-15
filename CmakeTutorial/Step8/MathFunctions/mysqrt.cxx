bfa6c6da4f3f2d2184b4b3d06c8b6a54fa3bf01f
chriswoodcn <chriswoodcn@aliyun.com>
2024-12-15 15:54:11 +0800

    [update] cmake 8

--- CmakeTutorial/Step8/MathFunctions/mysqrt.cxx
+++ CmakeTutorial/Step8/MathFunctions/mysqrt.cxx
@@ -1,25 +1,22 @@
 #include "mysqrt.h"
 
-#include <cmath>
 #include <iostream>
+#include "Table.h"
 
 namespace mathfunctions {
 namespace detail {
 // a hack square root calculation using simple operations
-double mysqrt(double x)
-{
+double mysqrt(double x) {
   if (x <= 0) {
     return 0;
   }
 
-  // if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-  double result = std::exp(std::log(x) * 0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result
-            << " using log and exp" << std::endl;
-#else
   double result = x;
-
+  if (x >= 1 && x < 10) {
+    std::cout << "Use the table to help find an initial value " << std::endl;
+    result = sqrtTable[static_cast<int>(x)];
+    return result;
+  }
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
     if (result <= 0) {
@@ -29,8 +26,8 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
-#endif
   return result;
 }
-}
-}
+
+} // namespace detail
+} // namespace mathfunctions
