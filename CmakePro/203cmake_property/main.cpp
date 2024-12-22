c1ac124f488c474bc46eb78e213a95f02c3c1309
chriswoodcn <chriswoodcn@aliyun.com>
2024-12-22 16:43:42 +0800

    [update] cmakepro 204

--- CmakePro/203cmake_property/main.cpp
+++ CmakePro/203cmake_property/main.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    cout<<"Test cmake property"<<endl;
-    cout<<"PARA1 = "<<PARA1<<endl;
-    return 0;
+int main() {
+  cout << "Test cmake property" << endl;
+#ifdef PARA1
+  cout << "PARA1 = " << PARA1 << endl;
+#endif
+#ifdef PARA2
+ cout << "PARA2 = " << PARA2 << endl;
+#endif
+  return 0;
 }
